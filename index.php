a1fd3764eab344c404d09fea9fa5b2d0c9d9a4e2
carlosura <cura03@gmail.com>
2024-08-19 22:01:19 -0500

    Se actualiza componente

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+include('mussio.php');
+include('luis.php');
+include ('abel.php');
 include('uziel.php');
 
 echo 'Hello world';
+
+echo abel();
+echo mussio();
+echo luis();
 echo carlos();
 
 ?>
